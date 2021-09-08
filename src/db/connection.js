443428ed4726a43746c8b681f4f6405815056118
Jordan Richmond <richmond42@ymail.com>
2021-09-08 11:58:04 +0100

    working

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,14 +1,21 @@
 const { MongoClient } = require("mongodb");
 require("dotenv").config()
 
-const client = new MongoClient(process.env.MONGO_URI)
+const client = new MongoClient(process.env.MONGO_URI);
 
 const connection = async (crudFunc, dataObj) => {
-    await client.connect();
-    const db = client.db("movies");
-    const collection = db.collection("movie")
-    await crudFunc(collection, dataObj)
-    client.close()
-}
+    try {
+        await client.connect();
+        const db = client.db("movies");
+        const collection = db.collection("movie")
+        // await crudFunc(collection, dataObj)
+        await collection.insertOne({name: "Steve"});
+        client.close();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+connection();
 
 module.exports = connection;
